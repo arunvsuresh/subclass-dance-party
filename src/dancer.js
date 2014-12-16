ee9c1409ce30dac305f3b191450ad5b89d33aa92
arunvsuresh <arunvsuresh@gmail.com>
2014-12-15 17:29:27 -0800

    Refactored Dancer and BlinkyDancer classes

--- src/dancer.js
+++ src/dancer.js
@@ -1,33 +1,40 @@
 // Creates and returns a new dancer object that can step
 var makeDancer = function(top, left, timeBetweenSteps){
-
-  var dancer = {};
-
+  this.timeBetweenSteps = timeBetweenSteps;
+  this.top=top;
+  this.left=left;
+  // var top = top;
+  // var left = left;
   // use jQuery to create an HTML <span> tag
-  dancer.$node = $('<span class="dancer"></span>');
+  //this.$node = $('<span class="dancer"></span>');
 
+  // now that we have defined the this object, we can start setting up important parts of it by calling the methods we wrote
+  // this one sets the position to some random default point within the body
+  this.setPosition(top, left);
+  //this.step();
+};
 
-  dancer.step = function(){
-    // the basic dancer doesn't do anything interesting at all on each step,
+makeDancer.prototype.constructor = makeDancer;
+makeDancer.prototype.step = function(){
+    // the basic this doesn't do anything interesting at all on each step,
     // it just schedules the next step
-    setTimeout(dancer.step, timeBetweenSteps);
-  };
-  dancer.step();
+    setTimeout(this.step, this.timeBetweenSteps);
+};
 
-  dancer.setPosition = function(top, left){
+makeDancer.prototype.$node = $('<span class="dancer"></span>');
+
+makeDancer.prototype.setPosition = function(top, left){
     // Use css top and left properties to position our <span> tag
     // where it belongs on the page. See http://api.jquery.com/css/
     //
+    var top, left;
+    (top===undefined) ? (top=this.top) : null;
+    (left===undefined) ? (left=this.left) : null;
+
     var styleSettings = {
       top: top,
       left: left
     };
-    dancer.$node.css(styleSettings);
-  };
-
-  // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
-  // this one sets the position to some random default point within the body
-  dancer.setPosition(top, left);
-
-  return dancer;
+    this.$node.css(styleSettings);
 };
+
